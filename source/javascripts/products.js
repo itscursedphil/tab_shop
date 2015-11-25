10ed4e3b01e132357eedec62c9e6c3bdbf9aff00
“itscursedphil <sayhi@itscursedphil.com>
2015-11-25 01:07:35 +0100

    Changed animation behavior on /products

--- source/javascripts/products.js
+++ source/javascripts/products.js
@@ -23,27 +23,27 @@ $(document).ready(function(){
 					oldPosition = position;
 				});
 			});
-			$('.product').each(function(){
-				var el = $(this);
-				var oldPosition = 0;
-				$(window).scroll(function(){
-					var offset = el.offset().top;
-					var position = offset - $(window).scrollTop();
-					var direction = function(){
-						if(oldPosition > position){
-							return 'up';
-						} else {
-							return 'down';
-						}
-					};
-					if(position < 100 && el.hasClass('anim-fadeout') === false && el.hasClass('anim-movedown') === false && direction() == 'up'){
-						el.addClass('anim-fadeout').addClass('anim-movedown');
-					} else if(position > 0 && el.hasClass('anim-fadeout') === true && el.hasClass('anim-movedown') === true && direction() == 'down'){
-						el.removeClass('anim-fadeout').removeClass('anim-movedown');
-					}
-					oldPosition = position;
-				});
-			});
+			// $('.product').each(function(){
+			// 	var el = $(this);
+			// 	var oldPosition = 0;
+			// 	$(window).scroll(function(){
+			// 		var offset = el.offset().top;
+			// 		var position = offset - $(window).scrollTop();
+			// 		var direction = function(){
+			// 			if(oldPosition > position){
+			// 				return 'up';
+			// 			} else {
+			// 				return 'down';
+			// 			}
+			// 		};
+			// 		if(position < 100 && el.hasClass('anim-fadeout') === false && el.hasClass('anim-movedown') === false && direction() == 'up'){
+			// 			el.addClass('anim-fadeout').addClass('anim-movedown');
+			// 		} else if(position > 0 && el.hasClass('anim-fadeout') === true && el.hasClass('anim-movedown') === true && direction() == 'down'){
+			// 			el.removeClass('anim-fadeout').removeClass('anim-movedown');
+			// 		}
+			// 		oldPosition = position;
+			// 	});
+			// });
 		}
 	}
 });
